4898ce534afc4d8687020b87640352b485a765db
Ireth <77365238+IrethLG@users.noreply.github.com>
2021-11-27 19:52:27 -0600

    Agregar archivos de proyecto.

---
+++ TiendaAPI/Controllers/ClienteController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TiendaAPI.Contexts;
+using TiendaAPI.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TiendaAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly AppDbContext context;
+
+        public ClienteController(AppDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<ClienteController>
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return   context.Cliente.ToList();
+        }
+
+        // GET api/<ClienteController>/5
+        [HttpGet("{id}")]
+        public Cliente Get(int id)
+        {
+             var cliente = context.Cliente.FirstOrDefault(p => p.idCliente == id);
+            return cliente;
+        }
+
+        // POST api/<ClienteController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClienteController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClienteController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TiendaAPI/Entities/Cliente.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaAPI.Entities
+{
+    public class Cliente
+    {
+        [Key]
+
+        public int idCliente
+        {
+            get; set;
+        }
+        public string nombre
+        {
+            get; set;
+        }
+        public string apellidoP
+        {
+            get; set;
+        }
+        public string apellidoM
+        {
+            get; set;
+        }
+        public string direccion
+        {
+            get; set;
+        }
+        public string telefono
+        {
+            get; set;
+        }
+
+        public bool estatus
+        {
+            get; set;
+        }
+    }
+}
